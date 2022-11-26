0fb0cdacf0afe9860c6d0f56e2169be2802f8267
Christian Winther <jippignu@gmail.com>
2022-11-26 23:24:44 +0100

    omptimize images
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- scripts/optimize-images.sh
+++ scripts/optimize-images.sh
@@ -1,1 +1,1 @@
-find docs -name "*.png" | xargs docker run --rm -t -v $PWD:/source luizeof/image-optimizer -o7 -simulate
+find docs -name "*.png" | xargs docker run --rm -t -v $PWD:/source luizeof/image-optimizer
