7b82730216a82fcbcbeb490b2e4707ad1ff45c97
Christian Winther <jippignu@gmail.com>
2022-12-02 18:04:16 +0100

    add variables
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- scripts/servers/monitoring/provider.tf
+++ scripts/servers/monitoring/provider.tf
@@ -6,6 +6,16 @@ terraform {
   }
 }
 
+variable "datadog_api_key" {
+  type        = string
+  description = "Datadog API Key"
+}
+
+variable "datadog_app_key" {
+  type        = string
+  description = "Datadog Application Key"
+}
+
 # Configure the Datadog provider
 provider "datadog" {
   api_key = var.datadog_api_key
