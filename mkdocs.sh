09932f4afb0f8edcdcadfbe93d053dbd27613f00
Christian Winther <jippignu@gmail.com>
2022-12-02 19:16:34 +0100

    use CI image
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# build
-docker build -t local-mkdocs .
+DOCKER_IMAGE="ghcr.io/jippi/masto-guide:main"
+
+if [ -z $CI ]
+then
+    DOCKER_IMAGE=local-mkdocs
+
+    # build
+    docker build -t local-mkdocs .
+fi
 
 # run
-exec docker run --name mkdocs-local --rm -p 8000:8000 -v ${PWD}:/docs local-mkdocs $@
+exec docker run --name mkdocs-local --rm -p 8000:8000 -v ${PWD}:/docs $DOCKER_IMAGE $@
